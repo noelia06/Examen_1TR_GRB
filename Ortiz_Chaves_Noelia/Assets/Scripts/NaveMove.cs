ae619f8299cf73b754d86180cb646460c4c92598
noelia06 <72862038+noelia06@users.noreply.github.com>
2020-12-09 18:16:27 +0100

    Movimiento nave
    He hecho el eje horizontal y vertical pero me falta restringir que no vaya para atrás y la rotación.

---
+++ Ortiz_Chaves_Noelia/Assets/Scripts/NaveMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveMove : MonoBehaviour
+{
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverNave();
+    }
+
+    void MoverNave()
+    {
+        float posX = transform.position.x;
+        float posZ = transform.position.z;
+      
+        float DesplZ = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * DesplZ);
+
+        float DesplX = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * DesplX);
+
+        if (posZ < 9.5f && posZ > -9.5f || posZ < -9.5f && desplZ > 0 || posZ > 9.5f && desplZ < 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed * desplZ);
+        }
+
+
+
+
+
+    }
+}
